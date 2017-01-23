5f1eb5030ad978b5879a9cab2863857db72971ab
Gambadeur <blardonemathieu@yahoo.fr>
2017-01-23 21:48:03 +0100

    ajout animation personnage avec la balise canvas et jquery

--- index.js
+++ index.js
@@ -23,3 +23,44 @@ document.querySelector("#excuses").innerHTML = "<div> " + personne + action + ob
 // var canvas = document.getElementById("canvas");
 //
 // canvas.ellipse(100, 100, 50, 75);
+
+// Store the canvas object into a variable
+var $myCanvas = $('#myCanvas');
+
+$myCanvas.drawArc({
+  // draw the face
+  fillStyle: 'white',
+  strokeStyle: '#333',
+  strokeWidth: 3,
+  x: 300, y: 100,
+  radius: 100
+}).drawArc({
+  // draw the left eye
+  fillStyle: '#333',
+  strokeStyle: '#333',
+  x: 250, y: 70,
+  radius: 5
+}).drawArc({
+  // draw the right eye
+  fillStyle: '#333',
+  strokeStyle: '#333',
+  x: 350, y: 70,
+  radius: 5
+}).drawArc({
+  // draw the nose
+  strokeStyle: '#333',
+  strokeWidth: 4,
+  ccw: true,
+  x: 300, y: 100,
+  radius: 30,
+  start: 0,
+  end: 10
+}).drawArc({
+  // draw the smile
+  strokeStyle: '#333',
+  strokeWidth: 4,
+  x: 300, y: 135,
+  radius: 30,
+  start: 90,
+  end: 280
+});
