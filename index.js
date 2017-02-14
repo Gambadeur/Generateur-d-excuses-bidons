043807b46fce32a1d174ac8a03f4b6de3cfa6d58
Gambadeur <blardonemathieu@yahoo.fr>
2017-02-14 15:05:15 +0100

    creation dat.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
 // Récupérer l'event click sur le bouton facon Vanilla Js
-// btn.addEventListener("click", function(){
-// alert("bibou");
-// });
-
+btn.addEventListener("click", function(){
+alert("bibou");
+});
 
 // show()
 // bottom circle:
@@ -12,7 +11,9 @@
 //
 // canvas.ellipse(100, 100, 50, 75);
 
+
 // Store the canvas object into a variable
+
 var $myCanvas = $('#myCanvas');
 
 var test = $myCanvas.children(3);
@@ -24,48 +25,51 @@ test2 = 40;
 
 
 // Récupérer l'event click sur le bouton facon Jquery
+
+
+// $("#btn").click(function(){
+//     console.log("hello");
+//     value += 10;
+//     function nose(value){
+//
+//
+// });
+
 var value = 20;
 
-$("#btn").click(function(){
-    console.log("hello");
-    value += 10;
-    function nose(value){
-    $myCanvas.drawArc({
-      // draw the face
-      fillStyle: 'white',
-      strokeStyle: '#333',
-      strokeWidth: 3,
-      x: 300, y: 100,
-      radius: 100
-    }).drawArc({
-      // draw the left eye
-      fillStyle: '#333',
-      strokeStyle: '#333',
-      x: 250, y: 70,
-      radius: 5
-    }).drawArc({
-      // draw the right eye
-      fillStyle: '#333',
-      strokeStyle: '#333',
-      x: 350, y: 70,
-      radius: 5
-    }).drawArc({
-      // draw the nose
-      fillStyle: '#333',
-      strokeStyle: '#333',
-      strokeWidth: 4,
-      ccw: true,
-      x: 300, y: 100,
-      radius: value
-    }).drawArc({
-      // draw the smile
-      strokeStyle: '#333',
-      strokeWidth: 4,
-      x: 300, y: 135,
-      radius: 30,
-      start: 90,
-      end: 280
-    });
-    }
-
+$myCanvas.drawArc({
+  // draw the face
+  fillStyle: 'white',
+  strokeStyle: '#333',
+  strokeWidth: 3,
+  x: 300, y: 100,
+  radius: 100
+}).drawArc({
+  // draw the left eye
+  fillStyle: '#333',
+  strokeStyle: '#333',
+  x: 250, y: 70,
+  radius: 5
+}).drawArc({
+  // draw the right eye
+  fillStyle: '#333',
+  strokeStyle: '#333',
+  x: 350, y: 70,
+  radius: 5
+}).drawArc({
+  // draw the nose
+  fillStyle: '#333',
+  strokeStyle: '#333',
+  strokeWidth: 4,
+  ccw: true,
+  x: 300, y: 100,
+  radius: value
+}).drawArc({
+  // draw the smile
+  strokeStyle: '#333',
+  strokeWidth: 4,
+  x: 300, y: 135,
+  radius: 30,
+  start: 90,
+  end: 280
 });
