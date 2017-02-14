7c269afd0a0e7b10ff45cb7f9c75ef0c48a7d0cc
Gambadeur <blardonemathieu@yahoo.fr>
2017-02-14 16:14:07 +0100

    animation canvas ok

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 // Récupérer l'event click sur le bouton facon Vanilla Js
-btn.addEventListener("click", function(){
-alert("bibou");
-});
+// btn.addEventListener("click", function(){
+//
+// });
 
 // show()
 // bottom circle:
@@ -34,9 +34,7 @@ test2 = 40;
 //
 //
 // });
-
 var value = 20;
-
 $myCanvas.drawArc({
   // draw the face
   fillStyle: 'white',
@@ -57,14 +55,6 @@ $myCanvas.drawArc({
   x: 350, y: 70,
   radius: 5
 }).drawArc({
-  // draw the nose
-  fillStyle: '#333',
-  strokeStyle: '#333',
-  strokeWidth: 4,
-  ccw: true,
-  x: 300, y: 100,
-  radius: value
-}).drawArc({
   // draw the smile
   strokeStyle: '#333',
   strokeWidth: 4,
@@ -73,3 +63,29 @@ $myCanvas.drawArc({
   start: 90,
   end: 280
 });
+
+var value = 10;
+
+function Excuse(){
+  value += 5;
+  $myCanvas.drawArc({
+    // draw the nose
+    name: 'nose',
+    fillStyle: '#333',
+    strokeStyle: '#333',
+    strokeWidth: 4,
+    ccw: true,
+    x: 300, y: 100,
+    radius: value
+  });
+  if (value === 100){
+    alert("Vous avez atteint votre limite d'excuses")
+    value = 10;
+  }
+
+  console.log(value);
+}
+
+// $myCanvas.animateLayer('nose', {
+//   radius: 70
+// })};
