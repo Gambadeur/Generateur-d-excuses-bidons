00c37d5f421e21b6159f11dce6858ec13792b4eb
Gambadeur <blardonemathieu@yahoo.fr>
2017-02-14 16:24:28 +0100

    cleanage des fichiers! tout est propre
    q
    wq:
    :

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
 
 // Récupérer l'event click sur le bouton facon Vanilla Js
-// btn.addEventListener("click", function(){
-//
-// });
+btn.addEventListener("click", function(){
+Excuse()
+NoseAnim()
+});
 
-// show()
-// bottom circle:
-
-// var canvas = document.getElementById("canvas");
-//
-// canvas.ellipse(100, 100, 50, 75);
-
-
-// Store the canvas object into a variable
+//Canvas petit bonhomme
 
 var $myCanvas = $('#myCanvas');
 
-var test = $myCanvas.children(3);
-console.log(test);
-var test2 = test.children(5);
-console.log(test2);
-test2 = 40;
-
-
-
-// Récupérer l'event click sur le bouton facon Jquery
-
-
-// $("#btn").click(function(){
-//     console.log("hello");
-//     value += 10;
-//     function nose(value){
-//
-//
-// });
 var value = 20;
 $myCanvas.drawArc({
   // draw the face
@@ -62,11 +37,22 @@ $myCanvas.drawArc({
   radius: 30,
   start: 90,
   end: 280
-});
+}).drawArc({
+    // draw the nose
+    name: 'nose',
+    fillStyle: '#333',
+    strokeStyle: '#333',
+    strokeWidth: 4,
+    ccw: true,
+    x: 300, y: 100,
+    radius: 10
+  });;
+
+// Canvas Nez du petit bonhomme
 
 var value = 10;
 
-function Excuse(){
+function NoseAnim(){
   value += 5;
   $myCanvas.drawArc({
     // draw the nose
@@ -81,11 +67,5 @@ function Excuse(){
   if (value === 100){
     alert("Vous avez atteint votre limite d'excuses")
     value = 10;
-  }
-
-  console.log(value);
+  };
 }
-
-// $myCanvas.animateLayer('nose', {
-//   radius: 70
-// })};
